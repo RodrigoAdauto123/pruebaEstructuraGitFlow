7bfba3f8bf0706fdba56e575818ca3a0460f7f19
RodrigoAOr <adauto.rodrigo@pucp.pe>
2022-06-17 16:47:37 -0500

    Initial Commit

---
+++ ProyectoEjemplo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProyectoEjemplo
+//
+//  Created by Rodrigo Alexander Adauto Ortiz on 17/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
